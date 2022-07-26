2f4ed5b0c9258ce9bc5d849f2ef6454c37cf3afb
Guilherme Takahashi <gui.yukio175@gmail.com>
2022-07-26 16:08:48 -0300

    feat: improve input component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,4 +1,13 @@
-import React, { InputHTMLAttributes, useEffect, useRef } from 'react';
+import
+  React,
+  {
+    useState,
+    InputHTMLAttributes,
+    useEffect,
+    useRef,
+    useCallback,
+  }
+from 'react';
 import { IconBaseProps } from 'react-icons';
 import { useField } from '@unform/core';
 
@@ -14,14 +23,12 @@ const Input: React.FC<IInputProps> = ({
   icon: Icon,
   ...rest
 }) => {
-  const inputRef = useRef(null);
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
 
-  const {
-    fieldName,
-    defaultValue,
-    error,
-    registerField
-  } = useField(name);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const { fieldName, defaultValue, error, registerField } = useField(name);
 
   useEffect(() => {
     registerField({
@@ -31,11 +38,22 @@ const Input: React.FC<IInputProps> = ({
     });
   }, [fieldName, registerField]);
 
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false);
+    setIsFilled(!!inputRef.current?.value);
+  }, []);
+
   return (
-    <Container>
+    <Container isFocused={isFocused} isFilled={isFilled}>
       {Icon && <Icon size={20}/>}
 
       <input
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
         defaultValue={defaultValue}
         ref={inputRef}
         {...rest}
--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,18 +1,33 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-const Container = styled.div`
+interface IContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+}
+
+const Container = styled.div<IContainerProps>`
   background-color: #232129;
   border-radius: 10px;
-  border: 2px solid #232129;
   padding: 16px;
   width: 100%;
   display: flex;
   align-items: center;
+
+  border: 2px solid #232129;
   color: #666360;
 
   & + div {
     margin-top: 8px;
   }
+
+  ${props => props.isFocused && css`
+    border-color: #ff9000;
+    color: #ff9000;
+  `}
+
+  ${props => props.isFilled && css`
+    color: #ff9000;
+  `}
 
   input {
     background-color: transparent;
